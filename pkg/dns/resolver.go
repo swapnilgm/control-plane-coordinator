e559ac1eacf27d2f83286abab9db856a7093192b
Swapnil Mhamane <swapnil.mhamane@sap.com>
2018-07-23 02:35:44 +0530

    Add cname resolving using dns library
    Signed-off-by: Swapnil Mhamane <swapnil.mhamane@sap.com>

--- pkg/dns/resolver.go
+++ pkg/dns/resolver.go
@@ -17,6 +17,10 @@ package dns
 import (
 	"fmt"
 	"net"
+	"os"
+	"time"
+
+	"github.com/miekg/dns"
 )
 
 // ValidateDNSWithIP verifies whether the <lookupDNS> resolves to <expectedIP>.
@@ -35,13 +39,54 @@ func ValidateDNSWithIP(lookupDNS string, expectedIP net.IP) (bool, error) {
 
 // ValidateDNSWithCname verifies whether the <lookupDNS> resolves to <expectedCname>.
 func ValidateDNSWithCname(lookupDNS string, expectedCname string) (bool, error) {
-	cname, err := net.LookupCNAME(lookupDNS)
-	if err != nil {
-		return false, fmt.Errorf("failed to lookup CNAME for endpoint %s: %v", lookupDNS, err)
+
+	conf, err := dns.ClientConfigFromFile("/etc/resolv.conf")
+	if err != nil || conf == nil {
+		fmt.Printf("Cannot initialize the local resolver: %s\n", err)
+		return false, err
+	}
+	localm := &dns.Msg{
+		MsgHdr: dns.MsgHdr{
+			RecursionDesired: true,
+		},
+		Question: make([]dns.Question, 1),
+	}
+	localm.Id = dns.Id()
+	localc := &dns.Client{
+		ReadTimeout: 5 * time.Second,
 	}
 
-	if cname == fmt.Sprintf("%s.", expectedCname) {
-		return true, nil
+	localm.SetQuestion(dns.Fqdn(lookupDNS), dns.TypeCNAME)
+
+	var in *dns.Msg
+	for _, server := range conf.Servers {
+		r, _, err := localc.Exchange(localm, server+":"+conf.Port)
+		if err != nil {
+			return false, err
+		}
+		if r == nil || r.Rcode == dns.RcodeNameError {
+			return false, err
+		}
+		if r.Rcode == dns.RcodeSuccess {
+			in = r
+			break
+		}
 	}
+	if in.Id != localm.Id {
+		fmt.Fprintf(os.Stderr, "Id mismatch\n")
+		return false, nil
+	}
+	for i, answer := range in.Answer {
+		ans, ok := answer.(*dns.CNAME)
+		if !ok {
+			return false, nil
+		}
+		fmt.Printf("Answer%d : %s", i, ans)
+		if ans.Target == dns.Fqdn(expectedCname) {
+			return true, nil
+		}
+
+	}
+
 	return false, nil
 }
